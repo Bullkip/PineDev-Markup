2328fe57902030d34fefe5d4c9c264100ee735e1
Bullkip <01sinister@mail.com>
2022-01-26 02:21:28 +0200

    fix header on scroll change

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
 'use strict'
 
+// burger
 const burgerBtn = document.querySelector('.burger__icon'),
   menu = document.querySelector('.header__navigation--mobile.burger__menu')
 
@@ -22,3 +23,22 @@ let burgerMenu = new Burger()
 document.addEventListener('click', function(event) {
   burgerMenu.handler(event, burgerBtn, 'change',menu)
 })
+
+// change header on scroll
+const headerHeight = document.querySelector('header').offsetHeight
+
+console.dir(headerHeight)
+
+window.onscroll = function() {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > headerHeight/2 || document.documentElement.scrollTop > headerHeight/2) {
+      document.querySelector('header').classList.add('active')
+   
+  } else {
+    document.querySelector('#header').classList.remove('active')
+      
+  }
+}
